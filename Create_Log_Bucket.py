a3eec5e5ec3db4c49eedd72dccbd9d94670023d2
emcilhenny <47047805+emcilhenny@users.noreply.github.com>
2019-04-24 11:21:22 -0400

    update install script and new python files

---
+++ Create_Log_Bucket.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 27 17:43:34 2019
+
+@author: AvantikaDG
+"""
+
+import boto
+import boto.s3.connection
+import boto.s3
+
+access_key = $OS_ACCESS_KEY
+secret_key = $OS_SECRET_KEY
+
+
+conn = boto.s3.connection.S3Connection(
+    aws_access_key_id=access_key,
+    aws_secret_access_key=secret_key,
+    port=443,
+    host='swift-kaizen.massopen.cloud',
+    is_secure=True,
+    calling_format=boto.s3.connection.OrdinaryCallingFormat())
+
+bucket = conn.create_bucket('log_bucket')
+bucket.configure_versioning(True)
+
---
+++ write_logs.py
@@ -0,0 +1,22 @@
+import boto
+import boto.s3.connection
+import boto.s3
+import logging
+import datetime 
+
+
+conn = boto.s3.connection.S3Connection(
+	aws_access_key_id=access_key,
+	aws_secret_access_key=secret_key,
+	port=443,
+	host='kzn-swift.massopen.cloud',
+	is_secure=True,
+	calling_format=boto.s3.connection.OrdinaryCallingFormat()) 
+
+logging.info('log upload to '+ 'log_bucket' + ' ' + str(datetime.datetime.now()))
+try:
+    bucket = conn.get_bucket('log_bucket')
+    k = bucket.new_key('log_'+str(datetime.datetime.now().date()))
+    k.set_contents_from_filename('/store_log/log_' + str(datetime.datetime.now().date()))
+except:
+    logging.error('upload_to_bucket() - file could not be uploaded to bucket')
