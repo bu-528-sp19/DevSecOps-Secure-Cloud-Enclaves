60890e5bcbe043b8d4c73764bfc6cd17b6371ef6
root <root@final-test-2.moclocal>
2019-05-02 17:17:21 +0000

    change key locations

--- ObjectStorageAPI.py
+++ ObjectStorageAPI.py
@@ -112,14 +112,23 @@ conn = boto.s3.connection.S3Connection(
 #secrets = os.popen('openstack --os-identity-api-version 3 --os-username '+os.environ.get('OS_USERNAME')+ ' --os-password '+os.environ.get('OS_PASSWORD')+' secret list').read()
 #listOfURLs=re.findall("(?P<url>https?://[^\s]+)", secrets)
 #recent=listOfURLs[-1]
-recent="https://kaizen.massopen.cloud:13311/v1/secrets/386e3d58-40cc-4820-afd8-44dc46d35bdd"
 
+#recent="https://kaizen.massopen.cloud:13311/v1/secrets/386e3d58-40cc-4820-afd8-44dc46d35bdd"
+with open('/inf/store_key.json','r') as file:
+    data = file.read().replace('\n','')
+
+import re
+q=re.findall("(?P<url>https?://[^\s]+)",data)
+ 
+recent=q[0][:-2]
+print(recent)
 #GET_COMMAND='openstack --os-identity-api-version 3 --os-username '+ os.environ.get('OS_USERNAME')+ ' --os-password '+os.environ.get('OS_PASSWORD')+' secret get '
 GET_COMMAND='openstack --os-identity-api-version 3 --os-username '+os.environ.get('OS_USERNAME')+ ' --os-password '+os.environ.get('OS_PASSWORD')+' secret get '
 
 stri=os.popen(GET_COMMAND + recent + " --payload").read()#.encode()
 stri=stri.split("|")
-
+print("Stri")
+print(stri)
 indxOfSecret = stri.index(" Payload ") + 1
 password=stri[indxOfSecret]
 password=password[1:-1]
--- write_logs.py
+++ write_logs.py
@@ -8,14 +8,25 @@ import uuid
 import pyAesCrypt
 access_key = os.environ.get('OS_ACCESS_KEY')
 secret_key = os.environ.get('OS_SECRET_KEY')
-recent="https://kaizen.massopen.cloud:13311/v1/secrets/386e3d58-40cc-4820-afd8-44dc46d35bdd"
 
+#recent="https://kaizen.massopen.cloud:13311/v1/secrets/386e3d58-40cc-4820-afd8-44dc46d35bdd"
+with open('/inf/log_key.json','r') as file:
+    data = file.read().replace('\n','')
+
+import re
+q=re.findall("(?P<url>https?://[^\s]+)",data)
+ 
+recent=q[0][:-2]
+
+print(recent)
+print(recent)
 #GET_COMMAND='openstack --os-identity-api-version 3 --os-username '+ os.environ.get('OS_USERNAME')+ ' --os-password '+os.environ.get('OS_PASSWORD')+' secret get '
 GET_COMMAND='openstack --os-identity-api-version 3 --os-username '+os.environ.get('OS_USERNAME')+ ' --os-password '+os.environ.get('OS_PASSWORD')+' secret get '
 buffSz=1024*64
 stri=os.popen(GET_COMMAND + recent + " --payload").read()#.encode()
 stri=stri.split("|")
-
+print("Stri")
+print(stri)
 indxOfSecret = stri.index(" Payload ") + 1
 password=stri[indxOfSecret]
 password=password[1:-1]
